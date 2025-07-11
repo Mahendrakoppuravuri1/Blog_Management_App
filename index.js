1a13048a6a03fa972fca3a2d79bc9774fd3a1195
Mahendrakoppuravuri1 <vk9659@srmist.edu.in>
2025-07-11 09:54:02 +0530

    Your commit message here

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const posts = [];
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { posts: posts });
+});
+
+app.get("/create", (req, res) => {
+    res.sendFile("C:/Users/Hai/Documents/web_development/Backend/capstone-project/Blog_Application/public/create.html");
+});
+
+app.post("/submit", (req, res) => {
+    posts.push(req.body);
+    res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+    const index = req.params.id;
+    console.log("Edit page requested for index:", index);
+    res.render("edit.ejs", { post: posts[index], index: index });
+});
+
+app.post("/edit/:id", (req, res) => {
+    const index = req.params.id;
+    posts[index] = { title: req.body.title, content: req.body.content };
+    res.redirect("/");
+});
+
+app.post("/delete/:id",(req,res)=>{
+    const index=req.params.id;
+    posts.splice(index,1);
+    res.render("index.ejs",{
+        posts:posts,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
